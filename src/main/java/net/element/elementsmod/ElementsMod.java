7667684665e4649732850307336ad4105c46de27
Maxfield Hutton <maxfieldhutton@gmail.com>
2023-03-22 21:18:27 -0400

    Initial Commit

---
+++ src/main/java/net/element/elementsmod/ElementsMod.java
@@ -0,0 +1,14 @@
+package net.element.elementsmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElementsMod implements ModInitializer {
+	public static final String MOD_ID = "elementsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/element/elementsmod/ElementsModClient.java
@@ -0,0 +1,10 @@
+package net.element.elementsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ElementsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
