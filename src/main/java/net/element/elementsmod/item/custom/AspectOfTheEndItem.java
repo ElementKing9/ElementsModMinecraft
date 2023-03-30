2ab7be2008207e9be8a5f369b824c9dadf36076e
Maxfield Hutton <maxfieldhutton@gmail.com>
2023-03-30 15:33:37 -0400

    Changed a whole lot of crap idk anymore

--- src/main/java/net/element/elementsmod/item/custom/AspectOfTheEndItem.java
+++ src/main/java/net/element/elementsmod/item/custom/AspectOfTheEndItem.java
@@ -1,34 +1,52 @@
 package net.element.elementsmod.item.custom;
 
+
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.Material;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.registry.tag.BlockTags;
 import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 
-public class AspectOfTheEndItem extends Item {
-    public AspectOfTheEndItem(Settings settings) {
-        super(settings);
+
+public class AspectOfTheEndItem extends SwordItem {
+    public AspectOfTheEndItem(ToolMaterial material, int attackDamage, float attackSpeed, Settings settings) {
+        super(material, attackDamage, attackSpeed, settings);
     }
+
+    public ActionResult UseOnBlock(ItemUsageContext context) {
+        return super.useOnBlock(context);
+    }
+
+
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         if(!world.isClient() && hand == Hand.MAIN_HAND) {
-            outputRandomNumber(user);
-            user.getItemCooldownManager().set(this, 20);
+            user.sendMessage(Text.of("Some Code Working"));
+            //user.teleport();
+            user.getItemCooldownManager().set(this, 120);
         }
 
         return super.use(world, user, hand);
     }
 
-    private void outputRandomNumber(PlayerEntity player) {
-        player.sendMessage(Text.literal("Your Number is " + getRandomNumber()));
-    }
-
-    private int getRandomNumber() {
-        return Random.createLocal().nextInt(10);
+    public float getMiningSpeedMultiplier(ItemStack stack, BlockState state) {
+        if (state.isOf(Blocks.COBWEB)) {
+            return 15.0f;
+        }
+        Material material = state.getMaterial();
+        if (material == Material.PLANT || material == Material.REPLACEABLE_PLANT || state.isIn(BlockTags.LEAVES) || material == Material.GOURD) {
+            return 1.5f;
+        }
+        return 1.0f;
     }
 }
