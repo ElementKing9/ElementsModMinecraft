ae16afc2f1944dd65c2385470668cd4714862cb5
Maxfield Hutton <maxfieldhutton@gmail.com>
2023-04-07 12:12:14 -0400

    Excluded some folders from the repo changed the mod group tab to display the right name

--- src/main/java/net/element/elementsmod/item/ModItemGroup.java
+++ src/main/java/net/element/elementsmod/item/ModItemGroup.java
@@ -4,11 +4,13 @@ import net.element.elementsmod.ElementsMod;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
 
     public static final ItemGroup ELEMENTSMODITEMGROUP = FabricItemGroup.builder(new Identifier(ElementsMod.MOD_ID, "elementsmodgroup"))
+            .displayName(Text.translatable("Elements Mod"))
             .icon(() -> new ItemStack(ModItems.ASPECT_OF_THE_END))
             .entries((context, entries) -> {
                 //ADD EVERY ITEM HERE
